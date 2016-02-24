12f87be1541c0c7ddb9ff3f1de69f6fe0ac25f49
puthier <puthier@gmail.com>
2016-02-24 12:30:27 +0100

    Added new rules

--- progs/chip_seq_se/snakefiles/rules/macs_rule.py
+++ progs/chip_seq_se/snakefiles/rules/macs_rule.py
@@ -3,10 +3,12 @@ rule macs14:
         output: "output/macs/{smp}/{smp}_sorted_peaks.bed"
         message: """--- Searching peaks with macs."""
         params: gs=config["macs14"]["genome_size"], \
-                ctrl_name=config["input"]
+                ctrl_name=config["input"],
+                bw=config["macs14"]["band_width"]
         shell: """
         cd output/macs/{wildcards.smp}
-        macs14    -t ../../bam/{wildcards.smp}_q30_rmDup.bam \
+        macs14 -t ../../bam/{wildcards.smp}_q30_rmDup.bam \
+            --bw {params.bw}  --nomodel False \
             -c ../../bam/{params.ctrl_name}_q30_rmDup.bam -f BAM \
             -n {wildcards.smp} -g {params.gs} &> {wildcards.smp}_peaks.bed.log
         sort -g -k5,5     {wildcards.smp}_peaks.bed > {wildcards.smp}_sorted_peaks.bed
