08f836d0b567b70e43a973a3d1e30ffb6d80e03f
puthier <puthier@gmail.com>
2016-04-26 21:18:07 +0200

    Change gene_id to gene_name in featureCounts

--- progs/rna_seq_str_pe/snakefiles/rules/fCounts_known_gene_rule.py
+++ progs/rna_seq_str_pe/snakefiles/rules/fCounts_known_gene_rule.py
@@ -3,7 +3,7 @@ rule fCounts_known_genes:
     output: "output/quantification_known_genes/gene_counts.txt","output/quantification_known_genes/gene_counts_mini.txt"
     threads: 1
     shell: """
-    featureCounts -p -s 2 -T 15 -t exon -g gene_id -a {input.gtf} -o {output[0]} {input.bam} &> {output[0]}.log
+    featureCounts -p -s 2 -T 15 -t exon -g gene_name -a {input.gtf} -o {output[0]} {input.bam} &> {output[0]}.log
     cut -f 1,7- {output[0]}| awk 'NR > 1' | awk '{{gsub("output/bam/","",$0); print}}' | \
      awk '{{if(NR==1){{gsub(".bam","",$0); print}}else{{print}} }}' > {output[1]}> {output[1]}
 """
