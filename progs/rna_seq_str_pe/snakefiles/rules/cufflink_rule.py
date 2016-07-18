1ab843ee9c3d0f358295d7aeff1fe40e2320812f
puthier <puthier@gmail.com>
2016-07-18 21:31:05 +0200

    Fixed {params.args}

--- progs/rna_seq_str_pe/snakefiles/rules/cufflink_rule.py
+++ progs/rna_seq_str_pe/snakefiles/rules/cufflink_rule.py
@@ -4,4 +4,4 @@ rule cufflinks:
     params: gtf=config["gtf"], args=config["cufflinks"]["args"]
     threads: config["cufflinks"]["threads"]
     message: "--- Searching novel transcript with cufflinks."
-    shell:  """cufflinks -g {params.gtf} -p {threads}  -o output/cufflinks/{wildcards.smp} {input.bam} 2>output/cufflinks/{wildcards.smp}/transcripts.gtf.log"""
+    shell:  """cufflinks {params.args}  -g {params.gtf} -p {threads}  -o output/cufflinks/{wildcards.smp} {input.bam} 2>output/cufflinks/{wildcards.smp}/transcripts.gtf.log"""
