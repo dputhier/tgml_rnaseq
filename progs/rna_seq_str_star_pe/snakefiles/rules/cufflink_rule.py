d3fc8ef4fe4ae3b88dd66485352789f2c9f79749
puthier <puthier@gmail.com>
2016-07-18 22:59:19 +0200

    added a dependency

--- progs/rna_seq_str_star_pe/snakefiles/rules/cufflink_rule.py
+++ progs/rna_seq_str_star_pe/snakefiles/rules/cufflink_rule.py
@@ -1,7 +1,7 @@
 rule cufflinks:
-    input:  bam="output/bam/{smp}.bam"
+    input:  bam="output/bam/{smp}.bam", bai="output/bam/{smp}.bam.bai"
     output: gtf="output/cufflinks/{smp}/transcripts.gtf"
     params: gtf=config["gtf"], args=config["cufflinks"]["args"]
     threads: config["cufflinks"]["threads"]
     message: "--- Searching novel transcript with cufflinks."
-    shell:  """cufflinks -g {params.gtf} -p {threads}  -o output/cufflinks/{wildcards.smp} {input.bam} 2>output/cufflinks/{wildcards.smp}/transcripts.gtf.log"""
+    shell:  """cufflinks {params.args} -g {params.gtf} -p {threads}  -o output/cufflinks/{wildcards.smp} {input.bam} 2>output/cufflinks/{wildcards.smp}/transcripts.gtf.log"""
