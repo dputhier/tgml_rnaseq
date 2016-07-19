576d764871d9380691ac3a0519fdbc75fa5646e4
puthier <puthier@gmail.com>
2016-07-19 17:43:09 +0200

    cd *star_pe*

--- progs/rna_seq_str_star_pe/snakefiles/rules/dag_rule.py
+++ progs/rna_seq_str_star_pe/snakefiles/rules/dag_rule.py
@@ -3,7 +3,7 @@ rule draw_dag:
     output: "output/report/dag.png"
     threads: 1
     shell: """
-    cd progs/rna_seq_str_pe
+    cd progs/rna_seq_str_star_pe
     mkdir -p ../../output/report/ 
     snakemake -s snakefiles/Snakefile.py --rulegraph | dot -T png > ../../output/report/rulegraph.png
     snakemake -s snakefiles/Snakefile.py --dag | dot -T png > ../../output/report/dag.png
