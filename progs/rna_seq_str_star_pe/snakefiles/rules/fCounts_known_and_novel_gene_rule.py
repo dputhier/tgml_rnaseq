1b0b60bd5140eb0f8e005d15325f4e539c32051a
puthier <puthier@gmail.com>
2016-07-27 13:36:49 +0200

    Deleted gsub("-","_",$0)

--- progs/rna_seq_str_star_pe/snakefiles/rules/fCounts_known_and_novel_gene_rule.py
+++ progs/rna_seq_str_star_pe/snakefiles/rules/fCounts_known_and_novel_gene_rule.py
@@ -7,5 +7,5 @@ rule fCounts_known_novel:
     shell: """
     featureCounts -p -s 2 -T 15 -t exon -g gene_name -a {input.gtf} -o {output[0]} {input.bam} &> {output[0]}.log
     cut -f 1,7- {output[0]}| awk 'NR > 1' | awk '{{gsub("output/bam/","",$0); print}}' | \
-    awk '{{if(NR==1){{gsub(".bam","",$0); gsub("-","_",$0); print}}else{{print}} }}' > {output[1]} 
+    awk '{{if(NR==1){{gsub(".bam","",$0); print}}else{{print}} }}' > {output[1]} 
 """
