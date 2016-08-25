b778bd1a89b45f86cc4552e9b7277bedfef4299d
puthier <puthier@gmail.com>
2016-08-25 18:41:19 +0200

    fixed folder in mapping stats

--- progs/rna_seq_str_star_pe/snakefiles/rules/mapping_stat_file_R1_rule.py
+++ progs/rna_seq_str_star_pe/snakefiles/rules/mapping_stat_file_R1_rule.py
@@ -1,6 +1,6 @@
 rule mapping_stats_R1:
-        input: raw="output/fastqc_raw/{smp}/{smp}_R1.fq_fastqc/fastqc_data.txt", \
-               trim="output/fastqc_trim/{smp}/{smp}_R1_t.fq_fastqc/fastqc_data.txt", \
+        input: raw="output/fastqc_raw/{smp}/{smp}_R1_fastqc/fastqc_data.txt", \
+               trim="output/fastqc_trim/{smp}/{smp}_R1_t_fastqc/fastqc_data.txt", \
                bam="output/bam/{smp}.bam"
         output: s="output/mapping_stats/{smp}_R1.stats", fs="output/mapping_stats/{smp}_R1.flagstat"
         message: """--- Performing some stats about mapping."""
