2059bee133dde801af594856c70ced9908634b5d
puthier <puthier@gmail.com>
2016-08-25 18:42:34 +0200

    fixed folder (fastqc) in Snakefile

--- progs/rna_seq_str_star_pe/snakefiles/Snakefile.py
+++ progs/rna_seq_str_star_pe/snakefiles/Snakefile.py
@@ -70,11 +70,11 @@ COMPARISON = config["comparison"].keys()
 #                         Workflow                               #
 #================================================================#
 
-FASTQC_RAW = expand("output/fastqc_raw/{smp}/{smp}_R1.fq_fastqc/fastqc_data.txt", smp=SAMPLES)
+FASTQC_RAW = expand("output/fastqc_raw/{smp}/{smp}_R1_fastqc/fastqc_data.txt", smp=SAMPLES)
 
 TRIMMING =  expand("output/trimmed/{smp}_R1_t.fq.gz", smp=SAMPLES)
 
-FASTQC_TRIM = expand("output/fastqc_trim/{smp}/{smp}_R1_t.fq_fastqc/fastqc_data.txt", smp=SAMPLES)
+FASTQC_TRIM = expand("output/fastqc_trim/{smp}/{smp}_R1_t_fastqc/fastqc_data.txt", smp=SAMPLES)
 
 MAPPING = expand("output/bam/{smp}.bam", smp=SAMPLES)
 
