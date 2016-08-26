9a5477dda6b9d307793306fd230e48a4fba6221d
puthier <puthier@gmail.com>
2016-08-26 15:23:03 +0200

    Fixed path in mapping stats

--- progs/rna_seq_str_star_pe/snakefiles/rules/mapping_stat_file_R2_rule.py
+++ progs/rna_seq_str_star_pe/snakefiles/rules/mapping_stat_file_R2_rule.py
@@ -1,6 +1,6 @@
 rule mapping_stats_R2:
-        input: raw="output/fastqc_raw/{smp}/{smp}_R2_fastqc/fastqc_data.txt", \
-               trim="output/fastqc_trim/{smp}/{smp}_R2_t_fastqc/fastqc_data.txt", \
+        input: raw="output/fastqc_raw/{smp}/{smp}_R2.fq_fastqc/fastqc_data.txt", \
+               trim="output/fastqc_trim/{smp}/{smp}_R2_t.fq_fastqc/fastqc_data.txt", \
                bam="output/bam/{smp}.bam"
         output: s="output/mapping_stats/{smp}_R2.stats", fs="output/mapping_stats/{smp}_R2.flagstat"
         message: """--- Performing some stats about mapping."""
