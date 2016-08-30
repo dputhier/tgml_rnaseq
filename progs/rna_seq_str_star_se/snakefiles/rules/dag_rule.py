a42f26cc7b0857c7d29752e692a4b128b9762e6a
puthier <puthier@gmail.com>
2016-08-30 11:25:44 +0200

    Fixed path in draw_dag

--- progs/rna_seq_str_star_se/snakefiles/rules/dag_rule.py
+++ progs/rna_seq_str_star_se/snakefiles/rules/dag_rule.py
@@ -1,9 +1,9 @@
 rule draw_dag:
-    input: "progs/rna_seq_str_star_pe/snakefiles/Snakefile.py"
+    input: "progs/rna_seq_str_star_se/snakefiles/Snakefile.py"
     output: "output/report/dag.png"
     threads: 1
     shell: """
-    cd progs/rna_seq_str_star_pe
+    cd progs/rna_seq_str_star_se
     mkdir -p ../../output/report/ 
     snakemake -s snakefiles/Snakefile.py --rulegraph | dot -T png > ../../output/report/rulegraph.png
     snakemake -s snakefiles/Snakefile.py --dag | dot -T png > ../../output/report/dag.png
