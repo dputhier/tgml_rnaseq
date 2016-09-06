6b8bae4a9cfb3f727a17c8cfb0346f7632599ae6
puthier <puthier@gmail.com>
2016-09-06 16:15:59 +0200

    Added bam_stat_by_chrom

---
+++ progs/rna_seq_str_star_pe/snakefiles/rules/bam_stat_by_chrom.py
@@ -0,0 +1,13 @@
+import os
+script = os.path.join(config["workingdir"], "progs", "scripts", "reads_per_chrom.py")
+
+
+rule bam_stat_by_chrom:
+        input: bam="output/bam/{smp}.bam"
+        output: o="output/bam_stat_by_chrom/{smp}_bam_stats.txt", d="output/bam_stat_by_chrom/{smp}_bam_stats.png"
+        params: script=script
+        message: """--- Performing some stats about mapping."""
+        threads: 1
+        shell:  """
+         {params.script} -i {input} -o {output.o} -d  {output.d} &> {output[0]}.log
+                """
