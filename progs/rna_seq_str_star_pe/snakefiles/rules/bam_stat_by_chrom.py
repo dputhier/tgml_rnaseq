200032940e7d3ebbc06e03dbbdcd9bc2964801b0
puthier <puthier@gmail.com>
2016-09-06 16:34:47 +0200

    Change message (by chrom)

--- progs/rna_seq_str_star_pe/snakefiles/rules/bam_stat_by_chrom.py
+++ progs/rna_seq_str_star_pe/snakefiles/rules/bam_stat_by_chrom.py
@@ -6,7 +6,7 @@ rule bam_stat_by_chrom:
         input: bam="output/bam/{smp}.bam"
         output: o="output/bam_stat_by_chrom/{smp}_bam_stats.txt", d="output/bam_stat_by_chrom/{smp}_bam_stats.png"
         params: script=script
-        message: """--- Performing some stats about mapping."""
+        message: """--- Performing some stats about mapping (by chrom)."""
         threads: 1
         shell:  """
          {params.script} -i {input} -o {output.o} -d  {output.d} &> {output[0]}.log
