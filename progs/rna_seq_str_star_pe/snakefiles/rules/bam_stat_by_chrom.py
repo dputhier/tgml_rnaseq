4cfe140dea7841b98cc696f88f3dac3cc065adc3
puthier <puthier@gmail.com>
2016-09-06 16:59:08 +0200

    add pythonpath var

--- progs/rna_seq_str_star_pe/snakefiles/rules/bam_stat_by_chrom.py
+++ progs/rna_seq_str_star_pe/snakefiles/rules/bam_stat_by_chrom.py
@@ -1,13 +1,14 @@
 import os
 script = os.path.join(config["workingdir"], "progs", "scripts", "reads_per_chrom.py")
+python_path = config["python2_path"]
 
 
 rule bam_stat_by_chrom:
         input: bam="output/bam/{smp}.bam"
         output: o="output/bam_stat_by_chrom/{smp}_bam_stats.txt", d="output/bam_stat_by_chrom/{smp}_bam_stats.png"
-        params: script=script
+        params: script=script, python_path=python_path
         message: """--- Performing some stats about mapping (by chrom)."""
         threads: 1
         shell:  """
-         {params.script} -i {input} -o {output.o} -d  {output.d} &> {output[0]}.log
+         {params.python_path} {params.script} -i {input} -o {output.o} -d  {output.d} &> {output[0]}.log
                 """
