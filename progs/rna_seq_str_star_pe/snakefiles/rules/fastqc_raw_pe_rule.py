75b35ed5c5e6210fa1c315168aef2f3bdac19936
puthier <puthier@gmail.com>
2016-09-08 10:09:29 +0200

    Getting crazy with fastqc output dir

--- progs/rna_seq_str_star_pe/snakefiles/rules/fastqc_raw_pe_rule.py
+++ progs/rna_seq_str_star_pe/snakefiles/rules/fastqc_raw_pe_rule.py
@@ -1,8 +1,8 @@
 rule fastqc_raw:
         input:  fwd="input/fastq/{smp}_R1.fq.gz", rev="input/fastq/{smp}_R2.fq.gz"
         threads: 1
-        output: fwd="output/fastqc_raw/{smp}/done_fwd", \
-                rev="output/fastqc_raw/{smp}/done_rev"
+        output: fwd="output/fastqc_raw/{smp}/{smp}_R1_fastqc/fastqc_data.txt", \
+                rev="output/fastqc_raw/{smp}/{smp}_R2_fastqc/fastqc_data.txt"
         message: """--- Quality check of raw data with Fastqc."""
         shell: """
             fastqc --outdir  output/fastqc_raw/{wildcards.smp} --extract  -f fastq {input.fwd} {input.rev} &> {output.fwd}.log 
