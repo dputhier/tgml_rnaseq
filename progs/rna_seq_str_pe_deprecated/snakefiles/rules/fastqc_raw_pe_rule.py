d3d776a03abf95dcaada68e7f9df16cefc39e150
Your Name <you@example.com>
2024-12-06 08:23:29 +0100

    remove deprecated

--- progs/rna_seq_str_pe_deprecated/snakefiles/rules/fastqc_raw_pe_rule.py
+++
@@ -1,8 +0,0 @@
-rule fastqc_raw:
-        input:  fwd="input/fastq/{smp}_R1.fq.gz", 
-                rev="input/fastq/{smp}_R2.fq.gz"
-        threads: 1
-        output: fwd="output/fastqc_raw/{smp}/{smp}_R1.fq_fastqc/fastqc_data.txt", \
-                rev="output/fastqc_raw/{smp}/{smp}_R2.fq_fastqc/fastqc_data.txt"
-        message: """--- Quality check of raw data with Fastqc."""
-        shell: "fastqc --outdir  output/fastqc_raw/{wildcards.smp} --extract  -f fastq {input.fwd} {input.rev} &> {output}.log "
